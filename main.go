e4ec0437d9ad6ae8e4f13d8226544b90cc7fb515
dont-fund-me <gabriel@dont-fund-me.org>
2025-11-05 02:39:52 -0800

    closer ..

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
 package main
 
-import "jam/internal/core"
-
 func main() {
-	core.Play(With())
+	play()
 }
-
-// less troubling. doesn't make me
-// think about how the gum is made
-// acceptable as walk and chew gum
