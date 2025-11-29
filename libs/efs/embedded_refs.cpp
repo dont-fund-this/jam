a042d35402ca1b083db666b4d25886c39cb13bcf
dont-fund-me <gabriel@dont-fund-me.org>
2025-11-28 21:03:13 -0800

    some embedded file system

--- libs/efs/embedded_refs.cpp
+++ libs/efs/embedded_refs.cpp
@@ -3,6 +3,7 @@
 #include "incbin.h"
 
 INCBIN(ref_refs_code__gitkeep, "../../refs/code/.gitkeep");
+INCBIN(ref_refs_code_code_zip, "../../refs/code/code.zip");
 INCBIN(ref_refs_data_sqlite_deploy_rc, "../../refs/data/sqlite/deploy.rc");
 INCBIN(ref_refs_data_sqlite_table_ais_tracks_sql, "../../refs/data/sqlite/table/ais_tracks.sql");
 INCBIN(ref_refs_data_sqlite_table_stop_times_sql, "../../refs/data/sqlite/table/stop_times.sql");
@@ -13,6 +14,7 @@ INCBIN(ref_refs_docs_read_md, "../../refs/docs/read.md");
 INCBIN(ref_refs_html_main_index_html, "../../refs/html/main/index.html");
 INCBIN(ref_refs_html_main_main_js, "../../refs/html/main/main.js");
 INCBIN(ref_refs_html_main_public_app_css, "../../refs/html/main/public/app.css");
+INCBIN(ref_refs_json__gitkeep, "../../refs/json/.gitkeep");
 INCBIN(ref_refs_misc__gitkeep, "../../refs/misc/.gitkeep");
 INCBIN(ref_refs_pdfs__gitkeep, "../../refs/pdfs/.gitkeep");
 INCBIN(ref_refs_pics__gitkeep, "../../refs/pics/.gitkeep");
@@ -21,6 +23,7 @@ INCBIN(ref_refs_yaml__gitkeep, "../../refs/yaml/.gitkeep");
 
 const char* embedded_files[] = {
     "code/.gitkeep",
+    "code/code.zip",
     "data/sqlite/deploy.rc",
     "data/sqlite/table/ais_tracks.sql",
     "data/sqlite/table/stop_times.sql",
@@ -31,6 +34,7 @@ const char* embedded_files[] = {
     "html/main/index.html",
     "html/main/main.js",
     "html/main/public/app.css",
+    "json/.gitkeep",
     "misc/.gitkeep",
     "pdfs/.gitkeep",
     "pics/.gitkeep",
@@ -47,6 +51,7 @@ struct EmbeddedFile {
 
 extern "C" const EmbeddedFile embedded_data[] = {
     {"code/.gitkeep", gref_refs_code__gitkeepData, gref_refs_code__gitkeepSize},
+    {"code/code.zip", gref_refs_code_code_zipData, gref_refs_code_code_zipSize},
     {"data/sqlite/deploy.rc", gref_refs_data_sqlite_deploy_rcData, gref_refs_data_sqlite_deploy_rcSize},
     {"data/sqlite/table/ais_tracks.sql", gref_refs_data_sqlite_table_ais_tracks_sqlData, gref_refs_data_sqlite_table_ais_tracks_sqlSize},
     {"data/sqlite/table/stop_times.sql", gref_refs_data_sqlite_table_stop_times_sqlData, gref_refs_data_sqlite_table_stop_times_sqlSize},
@@ -57,6 +62,7 @@ extern "C" const EmbeddedFile embedded_data[] = {
     {"html/main/index.html", gref_refs_html_main_index_htmlData, gref_refs_html_main_index_htmlSize},
     {"html/main/main.js", gref_refs_html_main_main_jsData, gref_refs_html_main_main_jsSize},
     {"html/main/public/app.css", gref_refs_html_main_public_app_cssData, gref_refs_html_main_public_app_cssSize},
+    {"json/.gitkeep", gref_refs_json__gitkeepData, gref_refs_json__gitkeepSize},
     {"misc/.gitkeep", gref_refs_misc__gitkeepData, gref_refs_misc__gitkeepSize},
     {"pdfs/.gitkeep", gref_refs_pdfs__gitkeepData, gref_refs_pdfs__gitkeepSize},
     {"pics/.gitkeep", gref_refs_pics__gitkeepData, gref_refs_pics__gitkeepSize},
